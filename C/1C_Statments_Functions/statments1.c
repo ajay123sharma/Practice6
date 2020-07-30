5ed172f8231a75ca626c3bf59f0dfb2e0d845e09
ajay123sharma <2015ajays@gmail.com>
2020-07-30 18:12:14 +0530

    day1 30 July
    Basic_Python.ipynb upload

---
+++ C/1C_Statments_Functions/statments1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	int number;
+	printf("Enter the number\n");
+	scanf("%d",&number);
+	printf("%d cube is\n", number*number*number);
+	return 0;
+}
---
+++ python/example.py
@@ -0,0 +1,6 @@
+lst = [1, 3, 4, 5, 1, 1, 1]
+print(lst.index(1))
+list1 = [1, 2, 3, 4, 1, 1, 1, 4, 5] 
+  
+# Will print the index of '4' in list1 
+print(list1.index(4)) 
